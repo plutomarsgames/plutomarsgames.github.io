c0049863c67b7d837da55971cc85ad5cef785f35
plutomarsgames <pluto17app@gmail.com>
2026-04-12 13:54:31 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore, doc, setDoc, onSnapshot, getDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getFirestore, doc, setDoc, onSnapshot, getDocs, collection } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAXs3JIC32b4JFhndDxtTptywQqSb5oeOw",
@@ -7,11 +7,10 @@ const firebaseConfig = {
   projectId: "plutomars-game",
   storageBucket: "plutomars-game.firebasestorage.app",
   messagingSenderId: "861771746833",
-  appId: "1:861771746833:web:c66f62ca1d589fb157abb0",
-  measurementId: "G-VQNM8H1FJF"
+  appId: "1:861771746833:web:c66f62ca1d589fb157abb0"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db, doc, setDoc, onSnapshot, getDoc };
+export { db, doc, setDoc, onSnapshot, getDocs, collection };
